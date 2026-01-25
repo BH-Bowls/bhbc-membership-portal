fc1be09fa93960dca343413821110f7e301cadf0
Liam Dasey <burgesshillbc@gmail.com>
2026-01-25 16:04:25 +0000

    Temp remove Friendlies and tea rota

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -190,15 +190,15 @@ export function Navbar({ userName, userRole, hasUnsavedChanges = false, actionBu
       ),
       subItems: adminMenuItems,
     }] : []),
-    {
-      name: 'Friendlies',
-      href: '/friendlies',
-      icon: (
-        <svg className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z" />
-        </svg>
-      ),
-    },
+//    {
+//      name: 'Friendlies',
+//      href: '/friendlies',
+//      icon: (
+//        <svg className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+//          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z" />
+//        </svg>
+//      ),
+//    },
     {
       name: 'Internal Games',
       href: '/internal-games',
@@ -218,7 +218,7 @@ export function Navbar({ userName, userRole, hasUnsavedChanges = false, actionBu
       subItems: [
         { name: 'Members', href: '/members' },
         { name: 'Clubs', href: '/clubs' },
-        { name: 'Tea Rota', href: '/tea-rota' },
+//        { name: 'Tea Rota', href: '/tea-rota' },
       ],
     },
 //    {
