1d4d391ca8b3abbfefb99f25105692efd8c9218d
Liam Dasey <burgesshillbc@gmail.com>
2026-04-10 08:13:50 +0100

    more PWA changes

--- app/kiosk/layout.tsx
+++
@@ -1,9 +0,0 @@
-import type { Metadata } from 'next';
-
-export const metadata: Metadata = {
-  manifest: '/manifest-kiosk.json',
-};
-
-export default function KioskLayout({ children }: { children: React.ReactNode }) {
-  return <>{children}</>;
-}
