cbfdd8d05c134bead093370ee99fde4b09bfe981
Liam Dasey <burgesshillbc@gmail.com>
2026-08-02 20:29:51 +0100

    1.25.1

--- src/config/version.ts
+++ src/config/version.ts
@@ -3,7 +3,7 @@
 // Update this file before deploying using: npm run update-version
 
 export const version = {
-  number: '1.25.0',
+  number: '1.25.1',
   buildDate: '2026-08-02', // This gets auto-updated by the update-version script
 } as const;
 
